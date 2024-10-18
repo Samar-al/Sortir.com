3c5a46b191b19f822b2ea995ee2a4c369c06dffa
Ismail GUNEY <ismail.guney2023@campus-eni.fr>
2024-10-18 13:38:46 +0200

    anonymise deleted participant functional

--- src/Controller/CityController.php
+++ src/Controller/CityController.php
@@ -14,11 +14,10 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use function PHPUnit\Framework\isEmpty;
 
-#[Route('/city')]
+#[Route('/ville')]
 final class CityController extends AbstractController
 {
 
-    #[isGranted('ROLE_ADMIN')]
     #[Route(name: 'app_city_index', methods: ['GET'])]
     public function index(CityRepository $cityRepository): Response
     {
@@ -60,19 +59,11 @@ final class CityController extends AbstractController
 
         return $this->render('city/new.html.twig', [
             'departments' => $departments,
-//            'cities' => $cities,
             'city' => $city,
             'formCity' => $formCity,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_city_show', methods: ['GET'])]
-    public function show(City $city): Response
-    {
-        return $this->render('city/show.html.twig', [
-            'city' => $city,
-        ]);
-    }
 
     #[Route('/get-cities/{id}', name: 'app_get_cities', methods: ['GET'])]
     public function getCities(string $id): JsonResponse
@@ -127,37 +118,5 @@ final class CityController extends AbstractController
 
         return json_decode($response, true);
     }
-
-
-//    #[Route('/{id}/edit', name: 'app_city_edit', methods: ['GET', 'POST'])]
-//    public function edit(Request $request, City $city, EntityManagerInterface $entityManager): Response
-//    {
-//        $form = $this->createForm(CityType::class, $city);
-//        $form->handleRequest($request);
-//
-//        if ($form->isSubmitted() && $form->isValid()) {
-//            $entityManager->flush();
-//
-//            return $this->redirectToRoute('app_city_index', [], Response::HTTP_SEE_OTHER);
-//        }
-//
-//        return $this->render('city/edit.html.twig', [
-//            'city' => $city,
-//            'form' => $form,
-//        ]);
-//    }
-
-//    #[Route('/{id}', name: 'app_city_delete', methods: ['POST'])]
-//    public function delete(Request $request, City $city, EntityManagerInterface $entityManager): Response
-//    {
-//        if ($this->isCsrfTokenValid('delete'.$city->getId(), $request->getPayload()->getString('_token'))) {
-//            $entityManager->remove($city);
-//            $entityManager->flush();
-//        }
-//
-//        return $this->redirectToRoute('app_city_index', [], Response::HTTP_SEE_OTHER);
-//    }
-
-
 
 }
